ed0eb9269e84ea3c8a1bd61a2c6a4c199e4b09d9
udhaya786 <udhayamoorthy@gmail.com>
2025-03-13 07:30:11 +0000

    Create JsonTest.cs

---
+++ JsonTest.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Converters;
+using System.IO;
+
+
+namespace InterviewTestQA
+{
+    public class JSONTest
+    { public string YearId { get; set; }
+    public int GeoRegionId { get; set; }
+    public int CountryId { get; set; }
+    public int RegionId { get; set; }
+    public int SchemeId { get; set; }
+    public int SchmTypeId { get; set; }
+    public double Cost { get; set; }
+}     
+}   
+
